4268a330751efe1c1698e2065f1e77d9456ae65d
Alexander Reisner <ga67wel@mytum.de>
2019-10-27 10:25:44 +0100

    Add php alert test

---
+++ alert.php
@@ -0,0 +1,5 @@
+<?php
+echo '<script language="javascript">';
+echo 'alert("SOFORTIGES HANDELN NOTWENDIG\n\nAuf ihrem iPhone wurde ein Trojaner gefunden (e.tre456_worm_iOS).\n\nBitte klicken Sie auf OK, um zu bestätigen, dass Ihnen diese Nachricht Angst macht.")';
+echo '</script>';
+?>
